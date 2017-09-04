7a8abb3ab8c8e5cdb4a3a37816bcdfae6dd10f09
shenbibo <402178946@qq.com>
2017-09-05 00:43:15 +0800

    上传简单lambad表达式测试

--- src/base/TestForKotlin.kt
+++ src/base/TestForKotlin.kt
@@ -10,14 +10,31 @@ package base
  * 命名参数测试
  */
 fun <T> Collection<T>.joinToString2(start: String = "[",
-                                 separator: String = ",",
-                                 end: String = "]"): String {
+                                    separator: String = ",",
+                                    end: String = "]"): String {
     val sb = StringBuilder()
     sb.append(start)
-    this.forEach { sb.append(it).append(separator) }
-    sb.append(end)
+    this.forEach { sb.append(it, separator) }
 
+//    sb.append(end)
+//    // 使用IntRanger
+//    return sb.removeRange(sb.lastIndex -1 until sb.lastIndex).toString()
 
-    // 使用IntRanger
-    return sb.removeRange(sb.lastIndex -1 until sb.lastIndex).toString()
+    // 或者使用以下方式
+    sb.deleteCharAt(sb.lastIndex)
+    return sb.append(end).toString()
+}
+
+/**
+ * 简单lambadas测试，从列表中过滤给定条件的数据
+ * */
+fun <T> Collection<T>.filterItem(condition: (T) -> Boolean) : Collection<T>{
+    val filterList = ArrayList<T>()
+    for(item in this){
+        if(condition(item)){
+            filterList.add(item)
+        }
+    }
+
+    return filterList
 }
--- src/test/TestForKotlinKtTest.kt
+++ src/test/TestForKotlinKtTest.kt
@@ -2,7 +2,9 @@ package test
 
 import org.junit.Test
 import base.joinToString2
+import base.filterItem
 import org.junit.Assert.*
+
 /**
  * 一句话描述
  *
@@ -12,10 +14,20 @@ class TestForKotlinKtTest {
     @Test
     fun joinToString() {
         val list = listOf(1, 2, 3, 4)
-
         assertEquals("[1,2,3,4]", list.joinToString2())
-        assertEquals("{1,2,3,4]", list.joinToString2 ("{"))
-        assertEquals("[1+2+3+4]", list.joinToString2 (separator = "+"))
+        assertEquals("{1,2,3,4]", list.joinToString2("{"))
+        assertEquals("[1+2+3+4]", list.joinToString2(separator = "+"))
+    }
+
+    @Test
+    fun filterItem() {
+        val strList = listOf("125", "sgsa", "Slg", "iir124", "a", "^&%", "A", "Ababc")
+
+        assertEquals("[sgsa]", strList.filterItem { it.startsWith('s') }.joinToString2())
+
+        assertEquals("[a,A,Ababc]",
+                strList.filterItem { it.startsWith('a', true) }.joinToString2())
+
     }
 
 }
